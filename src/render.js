0566921dfd67fbba67a325e00f2325a8cb39e909
koteech <nosenkoaa@mbsi.bz>
2022-04-27 23:15:16 +0300

    1.9. Время разбивать камни: - render.js - change insert DOM construction (switch to insertAdjacentElement)

--- src/render.js
+++ src/render.js
@@ -14,21 +14,21 @@ const createElement = (template) => {
 
 const render = (component, container, place = RenderPosition.BEFOREEND) => {
   const element = component.getElement();
-
-  switch (place) {
-    case RenderPosition.BEFOREBEGIN:
-      container.before(element);
-      break;
-    case RenderPosition.AFTERBEGIN:
-      container.prepend(element);
-      break;
-    case RenderPosition.BEFOREEND:
-      container.append(element);
-      break;
-    case RenderPosition.AFTEREND:
-      container.after(element);
-      break;
-  }
+  container.insertAdjacentElement(place, element);
+  // switch (place) {
+  //   case RenderPosition.BEFOREBEGIN:
+  //     container.before(element);
+  //     break;
+  //   case RenderPosition.AFTERBEGIN:
+  //     container.prepend(element);
+  //     break;
+  //   case RenderPosition.BEFOREEND:
+  //     container.append(element);
+  //     break;
+  //   case RenderPosition.AFTEREND:
+  //     container.after(element);
+  //     break;
+  // }
 };
 
 export {RenderPosition, createElement, render};
