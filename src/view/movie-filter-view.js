51b5314d1d43efdf7c5934fb6ab8bb0d66993fe5
koteech <nosenkoaa@mbsi.bz>
2022-05-15 17:08:34 +0300

    4.10. Шаблонизируй то - Описал фильтры понятнее

--- src/view/movie-filter-view.js
+++ src/view/movie-filter-view.js
@@ -1,19 +1,17 @@
 import AbstractView from '../framework/view/abstract-view';
 
-const getFilters = (movies) => {
-  const filtersMap = {
-    watchlist: (arr) => arr.filter((movie) => movie.userDetails.watchlist).length,
-    history: (arr) => arr.filter((movie) => movie.userDetails.alreadyWatched).length,
-    favorites: (arr) => arr.filter((movie) => movie.userDetails.favorite).length,
-  };
-
-  return Object.entries(filtersMap).map(([filterName, countMovies]) => (
-    {
-      name: filterName,
-      count: countMovies(movies),
-    }
-  ));
+const filtersMap = {
+  watchlist: (arr) => arr.filter((movie) => movie.userDetails.watchlist).length,
+  history: (arr) => arr.filter((movie) => movie.userDetails.alreadyWatched).length,
+  favorites: (arr) => arr.filter((movie) => movie.userDetails.favorite).length,
 };
+
+const getFilters = (films) => Object.entries(filtersMap).map(([filterName, countFilms]) => (
+  {
+    name: filterName,
+    count: countFilms(films),
+  }
+));
 
 const createFilterItemTemplate = (filter) => `<a href="#${filter.name}" class="main-navigation__item">${filter.name.charAt(0).toUpperCase() + filter.name.slice(1)} <span class="main-navigation__item-count">${filter.count}</span></a>`;
 
