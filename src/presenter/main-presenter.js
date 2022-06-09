d815973c8618cc2011188b953256e1cab26ffa36
koteech <nosenkoaa@mbsi.bz>
2022-06-09 14:57:01 +0300

    6.5. Обновление века (часть 1)

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -13,7 +13,9 @@ import ProfileView from '../view/header-profile-view';
 import ShowMoreButtonView from '../view/show-more-button-view';
 
 const ALL_MOVIE_COUNT_PER_STEP = 5;
+
 export const SECTION_EXTRA_TYPE = 'extra';
+
 export const SectionSettings = {
   ALL: {
     TITLE: 'All movies. Upcoming',
